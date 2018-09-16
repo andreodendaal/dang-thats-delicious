05fafe9c4ad97bfa3acbd8f30ebb109264fafae2
andre odendaal <aodendaal.direct@gmail.com>
2018-09-16 16:11:31 -0700

    Mongo connectivity

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
@@ -7,3 +10,11 @@ exports.addStore = (req, res) => {
   //res.render('editStore', {title: 'Add Store'});
   res.render('editStore', {title: 'Add Store'});
   };
+
+exports.createStore = async (req, res) => {
+  //console.log(req.body);
+  const store = new Store(req.body);
+  await store.save();
+  console.log('Store Saved!');
+  res.redirect('/');
+};
